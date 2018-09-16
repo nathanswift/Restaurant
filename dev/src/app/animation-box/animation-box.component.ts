51782ef224046ad513f93a1f9db281bbd2c498ca
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-09-16 17:50:49 -0600

    :bug: fixed connection issues - uneeded <p> tags added id to components

---
+++ dev/src/app/animation-box/animation-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animation-box',
+  templateUrl: './animation-box.component.html',
+  styleUrls: ['./animation-box.component.css']
+})
+export class AnimationBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -2,10 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { TesitimonialComponent } from './tesitimonial/tesitimonial.component';
+import { SliderComponent } from './slider/slider.component';
+import { BlogComponent } from './blog/blog.component';
+import { AnimationBoxComponent } from './animation-box/animation-box.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopComponent } from './top/top.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    MenuComponent,
+    TesitimonialComponent,
+    SliderComponent,
+    BlogComponent,
+    AnimationBoxComponent,
+    FooterComponent,
+    TopComponent,
+    TestimonialComponent,
   ],
   imports: [
     BrowserModule
